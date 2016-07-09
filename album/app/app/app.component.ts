b719aff4fdc983b80c24bd3a9573891935cdbf1e
DerfOh <fredrick.p@outlook.com>
2016-07-09 16:37:49 -0400

    Added more files for exeample

---
+++ album/app/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {HTTP_PROVIDERS, Http} from "@angular/http";
+import {DataTableDirectives} from 'angular2-datatable/datatable';
+import * as _ from 'lodash';
+
+
+@Component({
+    selector: 'album',
+    templateUrl: 'app/app.component.html',
+    providers: [HTTP_PROVIDERS],
+    directives: [DataTableDirectives],
+    pipes: [DatePipe]
+})
+export class AppComponent {
+
+    private data;
+
+    // Takes values placed in json file that correspond to the images
+    constructor(private http:Http) {
+       http.get("data/imageList.json")
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 1000);
+            });
+    }
+
+    //takes in a value and compares it to the url to see if there is a value specified there.
+    public checkHash(x){
+      var url = window.location.href;
+      var hashValue = url.substring(url.indexOf('#')+1);
+
+      if (x == hashValue){
+        return true;
+      }
+      return false;
+    }
+}
