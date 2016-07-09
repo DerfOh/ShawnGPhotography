4d62781ef512989831bc757cb7c460c4e69094bd
DerfOh <fredrick.p@outlook.com>
2016-07-09 11:43:48 -0400

    added dependency mappings to config.js

--- album/systemjs.config.js
+++ album/systemjs.config.js
@@ -8,7 +8,9 @@
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    'angular2-datatable':         'https://npmcdn.com/angular2-datatable@0.4.0',
+    'lodash':                     'https://npmcdn.com/lodash@4.6.1/lodash.js'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
