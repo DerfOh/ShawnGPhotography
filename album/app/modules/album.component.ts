c07ac2d1b4540440aa4cc7f7bc59461f5d6ddc58
DerfOh <fredrick.p@outlook.com>
2016-07-13 19:10:46 -0400

    update album app

--- album/app/modules/album.component.ts
+++ album/app/modules/album.component.ts
@@ -17,14 +17,45 @@ export class AlbumComponent {
     private data;
 
     // Takes values placed in json file that correspond to the images
+    // constructor(private http:Http) {
+    //    http.get("data/imageList.json")
+    //         .subscribe((data)=> {
+    //             setTimeout(()=> {
+    //                 this.data = data.json();
+    //             }, 1000);
+    //         });
+    // }
+
     constructor(private http:Http) {
-       http.get("data/imageList.json")
-            .subscribe((data)=> {
-                setTimeout(()=> {
-                    this.data = data.json();
-                }, 1000);
-            });
+       var url = window.location.href;
+       var hashValue = url.substring(url.indexOf('#')+1);
+       console.error(hashValue);
+
+       switch(hashValue) {
+            case "album1":
+                http.get("data/imageList.json")
+                 .subscribe((data)=> {
+                     setTimeout(()=> {
+                         this.data = data.json().album1;
+                     }, 1000);
+                 });
+                break;
+            case "album2":
+                http.get("data/imageList.json")
+                 .subscribe((data)=> {
+                     setTimeout(()=> {
+                         this.data = data.json().album2;
+                     }, 1000);
+                 });
+                break;
+            default:
+                console.error("Album not specified.");
+      }
+
+
     }
+
+
 
     //takes in a value and compares it to the url to see if there is a value specified there.
     public checkHash(x){
